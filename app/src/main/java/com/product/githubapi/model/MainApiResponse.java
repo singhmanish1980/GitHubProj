4a0d5f5a2851489b16b18b7afa6cebb33175319a
manish singh <manish.singh@sephora.com>
2020-10-20 19:56:17 -0500

    Model class created

---
+++ app/src/main/java/com/product/githubapi/model/MainApiResponse.java
@@ -0,0 +1,6 @@
+package com.product.githubapi.model;
+
+
+public interface MainApiResponse {
+
+}
---
+++ app/src/main/java/com/product/githubapi/model/MainModel.java
@@ -0,0 +1,9 @@
+package com.product.githubapi.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class MainModel {
+
+}
